90d8de10dda1083cf6a5f64cb441358c7d66d53f
In Lee <leein9404@naver.com>
2023-02-08 15:51:12 +0900

    시간 표시 기능 수정

---
+++ src/client/js/timeFormat.js
@@ -0,0 +1,54 @@
+const contentCreatedAt = document.getElementById("contentCreatedAt");
+const testClass = document.querySelectorAll(".contentCreatedAt");
+
+
+const timeFunc = (contentTime) => {
+    const present = Date.now();
+    const timeGap = present - Number(contentTime);
+    const timeList = [
+        [1000, 59], 
+        [60, 59], 
+        [60, 23], 
+        [24, 30],
+    ];
+    let result = timeGap;
+    let idx = 0;
+    for (let standardTime of timeList) {
+        if (result/standardTime[0] > standardTime[1]) {
+            result = Math.round(result/standardTime[0]);
+            ++idx
+        } else {
+            result = Math.round(result/standardTime[0]);
+            if (idx === 0) {
+                return "방금";
+            } else if (idx === 1) {
+                return `${Math.round(result)}분`;
+            } else if (idx === 2) {
+                return `${Math.round(result)}시간`;
+            } else {
+                return `${Math.round(result)}일`; 
+            }
+        }
+    }
+    const standardTimeObj = new Date(contentTime);
+    if (standardTimeObj.getFullYear() === present.getFullYear()) {
+        return `${standardTimeObj.getMonth()+1}.${standardTimeObj.getDate()}`
+    }
+    return `${standardTimeObj.getFullYear()}.${standardTimeObj.getMonth()+1}.${standardTimeObj.getDate()}`;
+}
+
+
+
+
+if (testClass.length === 0) {
+    contentCreatedAt.innerText = timeFunc(contentCreatedAt.innerText); 
+} else {
+    for (let tag of testClass) {
+        tag.innerText = timeFunc(tag.innerText);
+    } 
+}
+// let value = contentCreatedAt.innerText;
+
+// contentCreatedAt.addEventListener("scroll", function(value){
+//     contentCreatedAt.innerText = timeFunc(value);
+// });
